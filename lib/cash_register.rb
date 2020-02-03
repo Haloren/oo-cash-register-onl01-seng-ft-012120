6469db0d21f8c4f5f1fdf6cd9b517cf613a35187
Kyle McKenna <mckennakyler@gmail.com>
2020-02-03 18:15:49 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,79 +1,32 @@
-class CashRegister 
+class CashRegister
   
-  attr_accessor :total, :discount, :items, :price, :last_transaction # make an additional attr_accessor to keep track of last transaction 
+  attr_accessor :total, :discount, :item, :last_transaction
   
-  def initialize (discount = 0) # constant (optionally takes) discount = 0 
-    @total = 0 # sets an instance variable @total on initialization to zero
-    @discount = discount  # employee discount on initialization
-    @items = []  # items being purchased 
-  end 
-
-  # def total *un-needed method total is returned*
-  #   @total = 100 # returns the current @total spec folder shows = 100
-  # end 
-  
-  def add_item(title, price, qty = 1) # accepts a title and a price also accepts optional qty 
-    # if qty > 1 
-    #   i = 0
-    #   while i < qty
-    #   @items << title
-    #   i += 1 
-    #   end
-    # else 
-    #   @items << title 
-    # end
-    # @total += price * qty
-    # @last_transaction = @total
-    # @total 
-    
-    item = {} # make an item and give it a title, price, qty
-      item[:title] = title 
-      item[:price] = price 
-      item[:qty] = qty 
-    @items << item # each item is shoveled onto the @items 
-    @total += price * qty # doesn't forget about the previous total (total doesn't include the title) 
-    
-    @last_transaction = @total
-    
-    # @price = price
-    # @total += price * qty
-    # if qty > 1 
-    #   count_title = 0
-    #   while count_title < qty 
-    #     @items << title 
-    #     count_title += 1
-    #   end   
-    # else 
-    #   @items << title 
-    # end   
-  end
-  
-  def apply_discount # .to_f to make a float (a number with decimals) 
-    if @discount > 0 # applies the discount to the total price and reduces the total  
-      @discount /= 100.to_f 
-      @total -= (@total * @discount)
-      return "After the discount, the total comes to $800." # returns success message with updated total copied from spec folder 
-    else
-      return "There is no discount to apply." # returns a string error message that there is no discount to apply
-    end 
-  end 
-  
-  def items
-   all_items = [] # returns an array containing all items that have been added
-    @items.each do |item|
-      for qty in 1..item[:qty] #for in loop / times loop 
-        all_items << item[:title]
-      end 
+    def initialize(discount = 0)
+      @total = 0 
+      @discount = discount
+      @items = []
     end
-    all_items  
-  end
-  
-  def void_last_transaction() 
-    # if @items < 1   # returns the total to 0.0 if all items have been removed 
-    #   @total == 0.0 
-    # else 
-      @total -= @last_transaction # subtracts the last item from the total
-  #   end 
-  end
-  
-end 
+    
+    def add_item(title, price, qty = 1)
+      @total += price * qty
+      qty.times { @items << title }
+      @last_transaction = [title, price, qty]
+    end
+    
+    def apply_discount
+      @total == 0? "There is no discount to apply." : "After the discount, the total comes to $#{@total = @total * 80/100}."
+    end 
+    
+    def items
+      @items 
+    end 
+    
+    def void_last_transaction
+      @total -= @last_transaction[1] * @last_transaction[2]
+      @last_transaction[2].times do
+        @items.delete_at(@items.index(@last_transaction[0]) || @items.count)
+      end   
+    end       
+
+end
