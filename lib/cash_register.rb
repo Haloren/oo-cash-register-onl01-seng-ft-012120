44a2ffd5d9e68da8de7cd5fb34edb1254910fc97
Kyle McKenna <mckennakyler@gmail.com>
2020-02-04 18:29:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,45 +1,45 @@
-require 'pry'
+# require 'pry'
 
 
-class CashRegister
+# class CashRegister
   
-  attr_accessor :total, :discount, :items
-  attr_reader :last_transaction, :last_qty
-    def initialize(discount = 0)
-      @total = 0 
-      @discount = discount
-      @items = []
-    end
+#   attr_accessor :total, :discount, :items
+#   attr_reader :last_transaction, :last_qty
+#     def initialize(discount = 0)
+#       @total = 0 
+#       @discount = discount
+#       @items = []
+#     end
     
-    # def add_item(title, price, qty = 1)
-    #   @total += price * qty
-    #   qty.times { @items << title }
-    #   @last_transaction = [title, price, qty]
-    # end
+#     # def add_item(title, price, qty = 1)
+#     #   @total += price * qty
+#     #   qty.times { @items << title }
+#     #   @last_transaction = [title, price, qty]
+#     # end
     
-    def apply_discount
-      @total == 0? "There is no discount to apply." : "After the discount, the total comes to $#{@total = @total * 80/100}."
-    end 
+#     def apply_discount
+#       @total == 0? "There is no discount to apply." : "After the discount, the total comes to $#{@total = @total * 80/100}."
+#     end 
     
-    def total=(num)
-      @total = num.round(2)  
-    end
-    require 'pry'
-    def void_last_transaction
+#     def total=(num)
+#       @total = num.round(2)  
+#     end
+#     require 'pry'
+#     def void_last_transaction
       
-      #binding.pry
-      self.total -= @last_transaction
-      @last_qty.times{self.items.pop}
-    end
+#       #binding.pry
+#       self.total -= @last_transaction
+#       @last_qty.times{self.items.pop}
+#     end
     
-    def add_item(title, price, qty=1)
+#     def add_item(title, price, qty=1)
      
       
-      qty.times{items << title}  
-      self.total += price * qty
-      @last_transaction = price * qty
+#       qty.times{items << title}  
+#       self.total += price * qty
+#       @last_transaction = price * qty
     
-      @last_qty = qty
-    end
+#       @last_qty = qty
+#     end
 
-end
+# end
